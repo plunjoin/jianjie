41a19f26163612f505ae07081031736215fe6867
meijunhao <plunjoin@gmail.com>
2020-04-28 18:08:24 +0800

    修改造园的报幕

--- src/views/children/MakeChild.vue
+++ src/views/children/MakeChild.vue
@@ -3,33 +3,28 @@
     <div class="make-child-warp jie-container">
       <div class="banners">
         <img
-          v-if="b.bg_imgs"
+          v-if="b"
           v-lazy="b.bg_imgs?b.bg_imgs[0]+'?x-oss-process=image/resize,w_1920/quality,q_40':'https://www.jianjie-china.com/img/logo.bafa2e21.png?x-oss-process=image/resize,w_1920/quality,q_40'"
           alt
           srcset
         />
-        <div :class="'banner-title '+$i18n.locale">
+        <div :class="'banner-title '+$i18n.locale" v-if="b">
           <h2 class="jie-title-letter-spacing">{{ $i18n.locale!='en'?b.name:b.en_name }}</h2>
           <p>{{ $i18n.locale!='en'?b.subtitle:b.en_subtitle }}</p>
         </div>
       </div>
-      <div :class="'make-attributes '+$i18n.locale" v-if="b.info.length!=0">
-        <table style="width:100%;text-align:left;margin:2rem 0 3rem 0;">
-          <thead style="font-size:16px">
-            <th
-              class="jie-title-letter-spacing"
-              v-for="(e,i) in b.info"
-              :key="i"
-            >{{ $i18n.locale!='en'?e.title:e.en_title }}</th>
-          </thead>
-          <tbody style="font-size:16px">
-            <tr>
-              <td v-for="(e,i) in b.info" :key="i">{{ $i18n.locale!='en'?e.val:e.en_val }}</td>
-            </tr>
-          </tbody>
-        </table>
+      <div :class="'make-attributes '+$i18n.locale" v-if="b">
+        <div class="swiper-container">
+          <div class="swiper-wrapper info-wrap" v-if="b">
+          <dl class="swiper-slide info-item" v-for="(e,i) in b.info" :key="i">
+            <dt>{{ $i18n.locale!='en'?e.title:e.en_title }}</dt>
+            <dd>{{ $i18n.locale!='en'?e.val:e.en_val }}</dd>
+          </dl>
+          </div>
+        </div>
+      
       </div>
-      <div :class="'make-detail-content '+$i18n.locale">
+      <div :class="'make-detail-content '+$i18n.locale" v-if="b">
         <!-- <div style="display: flex;align-items: center;">
           <div style="text-align: center;">
             <p style="width: 50%;display: inline-block;text-align: left;">{{ $t('make.msg028') }}</p>
@@ -38,7 +33,7 @@
             <img src="../../assets/image/867ee26fb82863bf5ddf1730266567e.png" alt />
           </div>
         </div>-->
-        <div style="font-size: 16px;margin: 2rem 0;" v-if="b.materials">
+        <div style="font-size: 16px;margin: 2rem 0;" >
           <span style="margin: 0 0 10px 0;">{{ $t('make.msg029') }}</span>
           <p>{{ $i18n.locale!='en'?b.materials:b.en_materials }}</p>
         </div>
@@ -52,6 +47,7 @@
 </template>
 
 <script>
+import Swiper from "swiper/dist/js/swiper";
 import FOOT from "../../components/Footer";
 export default {
   name: "makechild",
@@ -64,7 +60,7 @@ export default {
     };
   },
   mounted() {
-    var _this = this;
+    var _this = this;    
     _this._id = window.location.href.split("?")[1];
     console.log(_this._id);
     _this.$store.state.All.buildgarden.forEach(e => {
@@ -73,7 +69,22 @@ export default {
       }
     });
     console.log(_this.b);
+    _this.$nextTick(()=>{
+      var swiper = new Swiper('.swiper-container',{
+          notNextTick: true, //notNextTick是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象，假如你需要刚加载遍使用获取swiper对象来做什么事，那么这个属性一定要是true
+          direction: "horizontal", //水平方向移动
+          grabCursor: true, //鼠标覆盖Swiper时指针会变成手掌形状，拖动时指针会变成抓手形状
+          setWrapperSize: true, //Swiper使用flexbox布局(display: flex)，开启这个设定会在Wrapper上添加等于slides相加的宽或高，在对flexbox布局的支持不是很好的浏览器中可能需要用到。
+          autoHeight: true, //自动高度。设置为true时，wrapper和container会随着当前slide的高度而发生变化
+          slidesPerView: 'auto', //设置slider容器能够同时显示的slides数量(carousel模式)。可以设置为数字（可为小数，小数不可loop），或者 'auto'则自动根据slides的宽度来设定数量。loop模式下如果设置为'auto'还需要设置另外一个参数loopedSlides。
+          mousewheel: true, //开启鼠标滚轮控制Swiper切换。可设置鼠标选项，默认值false
+          mousewheelControl: true, //同上
+          resistanceRatio: 0, //抵抗率。边缘抵抗力的大小比例。值越小抵抗越大越难将slide拖离边缘，0时完全无法拖离。本业务需要
+          observeParents: true, //将observe应用于Swiper的父元素。当Swiper的父元素变化时，例如window.resize，Swiper更新
 
+        })
+        console.log(swiper);
+    })
     // this.$axios.get("/buildgarden_single?parameter=5e86ef581eb9b40e3869920e");
   }
 };
@@ -92,6 +103,33 @@ export default {
   .make-attributes {
     width: 100%;
     overflow-x: auto;
+    .info-wrap{
+      margin:2rem 0 3rem 0;
+    }
+    .info-item{
+      font-size:.81rem;
+      text-align:center;
+      width:auto !important;
+      text-transform: capitalize;
+      margin:0 1rem;
+      dt{
+        font-weight:600;
+      }
+      dd{
+        font-size:.8rem;
+      }
+       &::after {
+        content: "|";
+        top: 50%;
+        right: -0.4rem;
+        position: absolute;
+      }
+      &:last-child {
+        &::after {
+          content: "";
+        }
+      }
+    }
     table {
       min-width: 1190px;
       td {
